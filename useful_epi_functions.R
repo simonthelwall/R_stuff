35d54507abbb2015ef4423da4ea3f906380998fa
simonthelwall <bobbins@bobbins.com>
2013-03-23 15:01:02 +0000

    add useful_epi_function.R

---
+++ useful_epi_functions.R
@@ -0,0 +1,45 @@
+# Test of two proportions. Supply two numerators and denominators.
+testTwoProps<-function(n1,d1,n2,d2){
+  p1<-n1/d1
+  p2<-n2/d2
+  pbar<-p1-p2
+  se<-sqrt(pbar*(1-pbar)*((1/d1)+(1/d2)))
+  cise<-sqrt(((p1*(1-p1))/d1)+(p2*(1-p2))/d2)
+  lci<-pbar-1.96*cise
+  uci<-pbar+1.96*cise
+  z<-pbar/se
+  p<-2*pnorm(-abs(z))
+  cat("Proportion 1:",round(p1,4),"\t Proportion 2:",round(p2,4),"\n",
+      "Difference of proportions:",round(pbar,4),"\n Std err of difference:",round(se,4),
+      "\n LCI of diff:",round(lci,4),"\t UCI of diff",round(uci,4),
+      "\n z-value:",round(z,4),"p-value",p,"\n\n")
+  }
+#testTwoProps(13,19,6,23)
+#should give 0.68, 0.26 and p~0.0057
+
+##Std error of the mean:
+sem<-function(x){
+  sd(x,na.rm=TRUE)/sqrt(length(x,na.omit=TRUE))
+}
+
+# Risk ratio
+riskRatio<-function(n1,d1,n2,d2){
+ r1<-n1/d1
+ r2<-n2/d2
+ rr<-r1/r2
+ return(rr)
+}
+#riskRatio(410,560,425,577)
+
+# Confidence interval for a risk ratio
+cirr<-function(n1,d1,n2,d2){
+  r1<-n1/d1
+  r2<-n2/d2
+  rr<-r1/r2
+  logrr<-log(rr)
+  se<-sqrt((1/n2)-(1/d2)+(1/n1)-(1/d1))
+  lci<-exp(logrr-(1.96*se))
+  uci<-exp(logrr+(1.96*se))
+  return(paste(round(lci,2),"-",round(uci,2),sep=""))
+}
+#serr(410,560,425,577)
