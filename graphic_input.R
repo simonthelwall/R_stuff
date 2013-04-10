408c339069c70556e0de20d9c9e74e4fb9a69693
Simon Thelwall <simon.thelwall@gmail.com>
2013-04-10 18:05:57 +0200

    Update graphic_input.R

--- graphic_input.R
+++ graphic_input.R
@@ -4,6 +4,12 @@ Sys.Date()
 dateDialog<-function(){
   datevar<-tclVar("")
   
+  # Create a variable to keep track of the state of the dialog window:
+  # done = 0; If the window is active
+  # done = 1; If the window has been closed using the OK button
+  # done = 2; If the window has been closed using the Cancel button or destroyed
+  done <- tclVar(0)
+  
   tt <- tktoplevel()
   tkwm.title(tt, "Date selection")
   newdate <- tkentry(tt, textvariable=datevar)
@@ -14,22 +20,27 @@ dateDialog<-function(){
   reset.but <- tkbutton(tt, text="Reset", command=reset)
   
   submit<-function(){
-    d <- as.Date(tclvalue(datevar), format="%d/%m/%Y")
-#    tkmessageBox(message=d)
+    results <- as.Date(tclvalue(newdate), format="%d/%m/%Y")
+    tclvalue(done) <- 1
+    #    tkmessageBox(message=d)
   }
   submit.but <- tkbutton(tt, text="submit", command=submit)
   
   quit.but <- tkbutton(tt, text="quit", 
                        command=function(){
-                         q(save="no")
-                         tkdestroy(tt)
+                         tclvalue(done) <- 2
                        })
+  
   tkgrid(tklabel(tt, text="Please enter date in ddmmyyyy format"), columnspan=3, pady = 10)
   tkgrid(tklabel(tt, text="Date:"), newdate, pady = 10, padx = 10)
   tkgrid(quit.but, reset.but, submit.but, pady = 10, padx = 10)
-  tkwait.window(tt)
+  tkfocus(tt)
+  tkwait.variable(done)
+  if(tclvalue(done)!=1){
+    results <- NULL
+  }
   tkdestroy(tt)
-  return(e=d)
+  return(results)
 }
 
 dateDialog()
