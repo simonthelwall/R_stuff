d50c39cdc821624c12f0b883961607b0c5f0d099
Simon Thelwall <simon.thelwall@gmail.com>
2013-08-30 10:29:27 +0100

    Added NHSvalidation2
    Added nested function to fix problems with NA or "" values in original function. 
    Nested function requires global variables.

--- NHS number validator.R
+++ NHS number validator.R
@@ -1,3 +1,50 @@
+NHSvalidation2 <- function(NHSnumber){
+  NHSnumber <- as.character(NHSnumber)
+  RV <<- NA
+  ifelse(
+    nchar(NHSnumber) != 10, RV <- 0, uniformN(NHSnumber)
+                 )
+  return(RV)
+}
+
+splitNHS <- function(NHSnumber){
+  a<-as.numeric(substr(NHSnumber,1,1))
+  b<-as.numeric(substr(NHSnumber,2,2))
+  c<-as.numeric(substr(NHSnumber,3,3))
+  d<-as.numeric(substr(NHSnumber,4,4))
+  e<-as.numeric(substr(NHSnumber,5,5))
+  f<-as.numeric(substr(NHSnumber,6,6))
+  g<-as.numeric(substr(NHSnumber,7,7))
+  h<-as.numeric(substr(NHSnumber,8,8))
+  i<-as.numeric(substr(NHSnumber,9,9))
+  j<-as.numeric(substr(NHSnumber,10,10))
+  return(list(a, b, c, d, e, f, g, h, i, j))
+}
+
+uniformN <- function(NHSnumber){
+#  print("uniformN called")
+  z <- splitNHS(NHSnumber)
+#  RV <- NA
+  ifelse( 
+    ( (z[[1]]==z[[2]]) & (z[[2]]==z[[3]]) & (z[[3]]==z[[4]]) & (z[[4]]==z[[5]]) & (z[[5]]==z[[6]]) & 
+        (z[[6]]==z[[7]]) & (z[[7]]==z[[8]]) & (z[[8]]==z[[9]]) & (z[[9]]==z[[10]]) ) , RV <<- 0, mo(NHSnumber)
+  )
+  return(RV)  
+}
+
+mo <- function(NHSnumber){
+#  print("mo called")
+  z <- splitNHS(NHSnumber)
+#  RV <- NA
+  Modulus<-((z[[1]]*10)+(z[[2]]*9)+(z[[3]]*8)+(z[[4]]*7)+(z[[5]]*6)+(z[[6]]*5)+(z[[7]]*4)+(z[[8]]*3)+(z[[9]]*2))
+  Modulus<-(11-(Modulus%%11))
+  RV <- "post modulus NA"
+  ifelse(
+    (Modulus==z[[10]]) | ((Modulus==11) & (z[[10]]==0)), RV <<- 1, RV <<- 0
+  )
+  return(RV)
+}
+
 NHSvalidation <- function(NHSnumber){
   
   NHSlength<-nchar(NHSnumber)
