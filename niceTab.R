f7567fdd12d512acf516d991a5bea36bbcf9cb3e
Simon Thelwall <bobbins@bobbins.com>
2014-02-08 22:29:49 +0000

    edits to niceTab
    Realised a major problem with the layout of the table. Not working at
    the moment.

--- niceTab.R
+++ niceTab.R
@@ -1,3 +1,5 @@
+#data(infert)
+# Use length of dimensions to correctly format rows and columns?
 niceTab <- function(v1, v2, data, percs.by = "row", chisq = FALSE){
   arguments = as.list(match.call())
   #print(arguments)
@@ -9,18 +11,32 @@ niceTab <- function(v1, v2, data, percs.by = "row", chisq = FALSE){
   col.names <-colnames(t)
   if(percs.by == "row"){
     row.sums <- apply(t, 1, sum)
+    # Display
+    cat("Cross-tabulation of", arguments$v1, " by ", arguments$v2, "\n")
+    cat(formatC("", width = 20), formatC(col.names, width = 10), "\n")
+    for(i in 1:dim(t)[1]){
+      row.pc <- (t[i, ]/row.sums[i])*100
+      cat(formatC(row.names[i], width = 10), formatC("n", width = 10), formatC(t[i,], width = 10), "\n")
+      cat(formatC("", width = 10),formatC("%", width = 10), formatC(row.pc, width = 10), "\n")
+      cat(formatC("", width = 10), rep("-----------", times = dim(t)[2]+1), "\n")
+    }
+  } else if (percs.by == "column") {
+    col.sums <- apply(t, 2, sum)
+    #Display
+    cat("Cross-tabulation of", arguments$v1, " by ", arguments$v2, "\n")
+    cat(formatC("", width = 10), formatC(col.names, width = 10), formatC("", width = 10),  "\n")
+    cat(formatC("", width = 10), formatC(col.names[1], width = 10),formatC("", width = 10), formatC(col.names[2], width = 10), "\n")
+    for(i in 1:dim(t)[1]){
+      col.pc <- (t[i, ]/col.sums[i])*100
+      cat(formatC(row.names[i], width = 10), formatC(t[i,], width = 10), formatC(col.pc, width = 10), "\n")
+      cat(formatC("", width = 10), rep("-----------", times = dim(t)[2]+1), "\n")
+    }
   }
-  # Display
-  cat("Cross-tabulation of", arguments$v1, " by ", arguments$v2, "\n")
-  cat(formatC("", width = 20), formatC(col.names, width = 10), "\n")
-  for(i in 1:dim(t)[1]){
-    row.pc <- (t[i, ]/row.sums[i])*100
-    cat(formatC(row.names[i], width = 10), formatC("n", width = 10), formatC(t[i,], width = 10), "\n")
-    cat(formatC("", width = 10),formatC("%", width = 10), formatC(row.pc, width = 10), "\n")
-    cat(formatC("", width = 10), rep("-----------", times = dim(t)[2]+1), "\n")
-  }
+  else 
+    cat("Please provide either 'row' or 'column' for perc.by, thanks.")
   
 }
 
-#data(infert)
-# niceTab(case, induced, infert)
+
+niceTab(induced, case, infert, percs.by = "row")
+niceTab(induced, case, infert, percs.by = "column")
